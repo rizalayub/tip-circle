41a37d7e5928660751a86c3bab66d7fdca7031a0
rizalayub <fordfocustpj@gmail.com>
2021-09-22 15:41:50 +0800

    upd

--- tipcircle.js
+++ tipcircle.js
@@ -1,12 +1,44 @@
-function converter(sentence){
-  let capitalized = []
-  let words = sentence.split(" ") //split the sentence into words
-  words.forEach(word => { 
-      let capitalizedWord = word.slice(0, 1).toUpperCase() + word.slice(1) //capitalize the first letter of every word
-      capitalized.push(capitalizedWord)         
-  })
-  let converted = capitalized.join(" ") 
-  return converted
-}
+import L from 'leaflet'
 
-module.exports = converter
+export var TipCircle = L.Circle.extend({
+  initialize: function(latlng, options, legacyOptions) {
+    // Create invisible marker
+    this._tip = L.circleMarker([0,0], {opacity: 0, radius: 0});
+    // Initialize as a normal Circle
+    L.Circle.prototype.initialize.call(this, latlng, options, legacyOptions);
+  },
+  redraw: function() {
+    L.Circle.prototype.redraw.call(this);
+    this._setTip();
+  },
+  onAdd: function() {
+    L.Circle.prototype.onAdd.call(this);
+    this._setTip();
+    this._tip.addTo(map);
+  },
+  onRemove: function() {
+    this._tip.remove();
+    L.Circle.prototype.onAdd.call(this);
+  },
+  _setTip: function() {
+    // Set the location for the tooltip to latitude of the bottom of the circle's
+    // bounding box, and the longitude of its centre.
+    this._tip.setLatLng([
+      this.getBounds()._southWest.lat,
+      this.getLatLng().lng]);
+    // Set the label to the circle's radius in metres
+    const tipText = String(this.getRadius());
+
+    // Remove any old tooltip and attach the new one
+    this._tip.unbindTooltip();
+    this._tip.bindTooltip(tipText, {
+                            direction: 'center',
+                            permanent: true,
+                            className: 'circleTip'
+                          });
+  }
+});
+
+export function tipCircle(latlng, options, legacyOptions) {
+  return new L.TipCircle(latlng, options, legacyOptions);
+}
