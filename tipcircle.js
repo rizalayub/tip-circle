bb1fdb4cd3f15eb663b5f8aa8f44a460c1bde264
rizalayub <fordfocustpj@gmail.com>
2021-09-22 15:54:27 +0800

    upd

--- tipcircle.js
+++ tipcircle.js
@@ -1,44 +1,62 @@
-import L from 'leaflet'
-
-export var TipCircle = L.Circle.extend({
-  initialize: function(latlng, options, legacyOptions) {
-    // Create invisible marker
-    this._tip = L.circleMarker([0,0], {opacity: 0, radius: 0});
-    // Initialize as a normal Circle
-    L.Circle.prototype.initialize.call(this, latlng, options, legacyOptions);
-  },
-  redraw: function() {
-    L.Circle.prototype.redraw.call(this);
-    this._setTip();
-  },
-  onAdd: function() {
-    L.Circle.prototype.onAdd.call(this);
-    this._setTip();
-    this._tip.addTo(map);
-  },
-  onRemove: function() {
-    this._tip.remove();
-    L.Circle.prototype.onAdd.call(this);
-  },
-  _setTip: function() {
-    // Set the location for the tooltip to latitude of the bottom of the circle's
-    // bounding box, and the longitude of its centre.
-    this._tip.setLatLng([
-      this.getBounds()._southWest.lat,
-      this.getLatLng().lng]);
-    // Set the label to the circle's radius in metres
-    const tipText = String(this.getRadius());
-
-    // Remove any old tooltip and attach the new one
-    this._tip.unbindTooltip();
-    this._tip.bindTooltip(tipText, {
-                            direction: 'center',
-                            permanent: true,
-                            className: 'circleTip'
-                          });
+(function (factory) {
+  if (typeof define === "function" && define.amd) {
+    // AMD
+    define(["leaflet"], factory);
+  } else if (typeof module !== "undefined" && typeof require !== "undefined") {
+    // Node/CommonJS
+    module.exports = factory(require("leaflet"));
+  } else {
+    // Browser globals
+    if (typeof window.L === "undefined") {
+      throw "Leaflet must be loaded first";
+    }
+    factory(window.L);
   }
+})(function (L) {
+  //L.SemiCircle = L.Circle.extend(semicircle);
+  L.TipCircle = L.Circle.extend({
+    initialize: function (latlng, options, legacyOptions) {
+      // Create invisible marker
+      this._tip = L.circleMarker([0, 0], { opacity: 0, radius: 0 });
+      // Initialize as a normal Circle
+      L.Circle.prototype.initialize.call(this, latlng, options, legacyOptions);
+    },
+    redraw: function () {
+      L.Circle.prototype.redraw.call(this);
+      this._setTip();
+    },
+    onAdd: function () {
+      L.Circle.prototype.onAdd.call(this);
+      this._setTip();
+      this._tip.addTo(map);
+    },
+    onRemove: function () {
+      this._tip.remove();
+      L.Circle.prototype.onAdd.call(this);
+    },
+    _setTip: function () {
+      // Set the location for the tooltip to latitude of the bottom of the circle's
+      // bounding box, and the longitude of its centre.
+      this._tip.setLatLng([
+        this.getBounds()._southWest.lat,
+        this.getLatLng().lng,
+      ]);
+      // Set the label to the circle's radius in metres
+      const tipText = String(this.getRadius());
+
+      // Remove any old tooltip and attach the new one
+      this._tip.unbindTooltip();
+      this._tip.bindTooltip(tipText, {
+        direction: "center",
+        permanent: true,
+        className: "circleTip",
+      });
+    },
+  });
+
+  L.tipCircle = function (latlng, options) {
+    return new L.TipCircle(latlng, options);
+  };
 });
 
-export function tipCircle(latlng, options, legacyOptions) {
-  return new L.TipCircle(latlng, options, legacyOptions);
-}
+
